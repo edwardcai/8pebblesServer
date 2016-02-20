e82f57f9f52c9a733eb950e6739d11d61ddfa52a
edwardcai <edward.h.cai@gmail.com>
2016-02-20 17:31:29 -0500

    json for pebbles

--- app.js
+++ app.js
@@ -13,15 +13,47 @@ app.get('/', function(req, res){
 
 app.use(express.static(path.join(__dirname + '/public')));
 
-app.get('/state', function(req, res){
-	res.json({"Nick":"Does This Work?"});
-});
+var message = "00000000"
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('message', function(msg){
+    io.emit('message', msg);
+		message = msg;
   });
 });
+
+app.get('/state0', function(req, res){
+	res.json({"Nick":message.charAt(0)});
+});
+
+app.get('/state1', function(req, res){
+	res.json({"Nick":message.charAt(1)});
+});
+
+app.get('/state2', function(req, res){
+	res.json({"Nick":message.charAt(2)});
+});
+
+app.get('/state3', function(req, res){
+	res.json({"Nick":message.charAt(3)});
+});
+
+app.get('/state4', function(req, res){
+	res.json({"Nick":message.charAt(4)});
+});
+
+app.get('/state5', function(req, res){
+	res.json({"Nick":message.charAt(5)});
+});
+
+app.get('/state6', function(req, res){
+	res.json({"Nick":message.charAt(6)});
+});
+
+app.get('/state7', function(req, res){
+	res.json({"Nick":message.charAt(7)});
+});
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
--- public/js/index.js
+++ public/js/index.js
@@ -8,25 +8,11 @@ function drawCircle(x,y,isFilled) {
 	ctx.closePath();
 }
 
-$(function(){
-	var iosocket = io.connect();
-	iosocket.on('connect', function () {
-		$('#incomingChatMessages').append($('<li>Connected</li>'));
-		iosocket.on('message', function(message) {
-			alert("hi");
-			$('#incomingChatMessages').append($('<li></li>').text(message));
-			drawCircle(50, 95, true);
-		});
-		iosocket.on('disconnect', function() {
-			$('#incomingChatMessages').append('<li>Disconnected</li>');
-		});
-	});
-	$('#outgoingChatMessage').keypress(function(event) {
-		if(event.which == 13) {
-			event.preventDefault();
-			iosocket.send($('#outgoingChatMessage').val());
-			$('#incomingChatMessages').append($('<li></li>').text($('#outgoingChatMessage').val()));
-			$('#outgoingChatMessage').val('');
-		}
-	});
+var socket = io();
+$('form').submit(function(){
+	socket.emit('message', $('#m').val());
+	$('#m').val('');
+	return false;
+});
+socket.on('message', function(msg){
 });
